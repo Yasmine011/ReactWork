25021b32d0bb1e3a4365b65121b10f0cfacf2775
Yasmine  Tillman <87497186+Yasmine011@users.noreply.github.com>
2021-09-07 17:04:14 -0500

    Add files via upload

---
+++ grocery-list/App.js
@@ -0,0 +1,16 @@
+import  FormControlClass from './form'
+import  './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+<h3 className= "Name"> Grocery App</h3>
+
+    <FormControlClass/>
+    </div>
+  );
+}
+
+export default App;
